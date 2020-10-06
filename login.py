e8d1e170d74058f951f850a2948c0c6ba8363eb4
km3560 <km3560@users.noreply.github.com>
2020-10-06 18:50:43 -0300

    Enable main function2

--- login.py
+++ login.py
@@ -14,13 +14,9 @@ class login:
 
     def credentials():
         chave_consumidor = SECRET_KEY
-        print(chave_consumidor)
         segredo_consumidor = KEY_COSTUMER
-        print(segredo_consumidor)
         token_acesso = ACESS_TOKEN
-        print(ACESS_TOKEN)
         token_acesso_segredo = SECRET_TOKEN_ACESS
-        print(token_acesso_segredo)
         autenticacao = tweepy.OAuthHandler(
             chave_consumidor, segredo_consumidor)
         autenticacao.set_access_token(token_acesso, token_acesso_segredo)
